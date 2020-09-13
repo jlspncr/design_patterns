aecec0d3740bf42940408d23fc752c47b6ebc106
jlspncr <jlspncer@gmail.com>
2020-09-13 14:14:47 -0400

    Reformat Program.cs

--- factory_method_pattern/Program.cs
+++ factory_method_pattern/Program.cs
@@ -11,6 +11,8 @@ namespace factory_method_pattern
       contracts[0] = new DoT();
       contracts[1] = new Speedway();
 
+      Console.WriteLine("");
+
       foreach (Contractor contract in contracts)
       {
         Console.WriteLine("\n" + "A New " + contract.GetType().Name + " project includes the following: ");
